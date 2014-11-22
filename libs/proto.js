0196755bdccdf422bf4900ce6949b99dbd831723
evanboissonnot <evan.boissonnot@netil.fr>
2014-11-22 12:54:05 +0100

    travail sur le proto

---
+++ libs/proto.js
@@ -0,0 +1,19 @@
+﻿var request = require('google-oauth-jwt');
+
+var app = module.exports = {};
+
+// environment
+
+var env = process.env.NODE_ENV || 'development';
+
+app.prepare = function() {
+	request.authenticate({
+        email: '953651403781-7a94m3crdala8d87imj6t3j5od67t1e9@developer.gserviceaccount.com',
+        keyFile: 'your-key-file.pem',
+        scopes: ['https://spreadsheets.google.com/feeds']
+    }, function (err, token) {
+        if (!err) {
+			
+		}
+	});
+}
---
+++ libs/spreadsheetpowa.js
@@ -0,0 +1,9 @@
+﻿var EventEmitter = require('events').EventEmitter;
+var merge = require('utils-merge');
+var proto = require('./proto');
+
+module.exports = create_powa;
+
+function create_powa() {
+	merge(app, EventEmitter.prototype);
+}
